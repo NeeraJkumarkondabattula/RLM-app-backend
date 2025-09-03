eda4b13d573b27f95fb8ef152c8d6e1a23b23877
NeeraJkumarkondabattula <neerajkumarkondabattula@gmail.com>
2025-09-03 16:17:32 +0530

    user schema updated commit

--- models/userModel.js
+++ models/userModel.js
@@ -1,8 +1,12 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
+  firstname: { type: String, required: true },
+  lastname: { type: String, required: true },
+  dob: { type: Date, required: true },
+  mobile: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String }, // optional
+  password: { type: String },
 });
 
 module.exports = mongoose.model("User", userSchema);
